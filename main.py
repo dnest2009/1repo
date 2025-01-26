69d8780c35b55aa785327b8fec40aecd626e6667
dnest2009 <dnest2009@gmail.com>
2025-01-26 17:41:47 +0200

    test

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("Hello World")
-print('hello git')
+# print("Hello World")
+# print('hello git')
+# class Parent:
+#   def __init__(self, txt):
+#     self.message = txt
+
+#   def printmessage(self):
+#     print(self.message)
+
+# class Child(Parent):
+#   def __init__(self, txt):
+#     super().__init__(txt)
+
+# x = Child("Hello, and welcome!")
+
+# x.printmessage()
+
+import time
+from datetime import date
+today = date.today()
+print(today)
+today = today.replace(year=today.year + 1)
+print(today)
