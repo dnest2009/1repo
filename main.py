1d81b8c483dd6bb8604a1d2c06f3b8785f036609
dnest2009 <dnest2009@gmail.com>
2025-02-18 13:25:09 +0200

    first_commit

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
-# print("Hello World")
-# print('hello git')
-# class Parent:
-#   def __init__(self, txt):
-#     self.message = txt
+from test import Item
 
-#   def printmessage(self):
-#     print(self.message)
+from collections import UserDict
 
-# class Child(Parent):
-#   def __init__(self, txt):
-#     super().__init__(txt)
+class LookUpKeyDict(UserDict):
+    def lookup_key(self, value):
+        keys = []
+        for key in self.data:
+            if self.data[key] == value:
+                keys.append(key)
+        return keys
+    
 
-# x = Child("Hello, and welcome!")
+d = LookUpKeyDict()
+d['a']= 1
+d['b'] = 2
+d['cf'] = 109
 
-# x.printmessage()
 
-import time
-from datetime import date
-today = date.today()
-print(today)
-today = today.replace(year=today.year + 1)
-print(today)
+class IDException(Exception):
+    pass
+
+
+def add_id(id_list, employee_id):
+    if not employee_id.startswith('01'):
+        raise IDException
+    else:
+        id_list.append(employee_id)
+    return id_list
+
+
+class Contacts:
+    current_id = 1
+
+    def __init__(self):
+        self.contacts = []
+
+    def list_contacts(self):
+        return self.contacts
+
+    def add_contacts(self, name, phone, email, favorite):
+        self.contacts.append({"id": Contacts.current_id,"name": name, "phone":phone, "email":email,"favorite": favorite})
+        #self.contacts.append()
+        Contacts.current_id += 1
+        return self.contacts
+
+lust = Contacts()
+x = {'name': 'Wylie Pope', 'phone': '(692) 802-2949', 'email': 'est@utquamvel.net', 'favorite': True}
+y = {'name': 'Cyrus Jackson', 'phone': '(501) 472-5218', 'email': 'nibh@semsempererat.com', 'favorite': False}
+print(lust.add_contacts(1,2,3,True))
+
